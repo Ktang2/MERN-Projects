d9af487edbee03ea1ca7f6d9b0f44f19a30ee87e
Kalvin Tang <kaltang@kalvins-mbp.mynetworksettings.com>
2023-11-16 14:16:06 -0500

    testing

--- box-generator/src/components/Display.jsx
+++ box-generator/src/components/Display.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Display = (props) => {
     const colors = props.allColors;
     return (
-        <div style={{display: 'flex', flexWrap:'wrap', width: 800}}>
+        <div style={{display: 'flex', flexWrap:'wrap', width: 800, marginTop:30}}>
             { colors.map( (oneColor, idx) => 
                 <div key={idx} style={{width: 100, height: 100,  marginRight: 10, marginBottom: 10, backgroundColor: oneColor}}></div>
             )}
