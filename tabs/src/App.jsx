0dc2aeddb74b3b5725a50291d379ea74edb3a88c
Kalvin Tang <kaltang@kalvins-mbp.mynetworksettings.com>
2023-12-05 20:40:07 -0500

    changed content

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -3,9 +3,9 @@ import Tabs from './components/tabs'
 
 function App() {
   const allTabs = [
-    {title: "Tab 1", content: "Content 1"},
-    {title: "Tab 2", content: "Content 2"},
-    {title: "Tab 3", content: "Content 3"}
+    {title: "Tab 1", content: "Display content specific to the first tab"},
+    {title: "Tab 2", content: "Display content specific to the second tab"},
+    {title: "Tab 3", content: "Display content specific to the third tab"}
   ]
 
   return (
